11613911256943cbc631d5f9950bcd8bdd7a95f5
lubcountcooper <lub@countcooper.nl>
2019-04-03 18:31:56 +0800

    enable editing and saving user profile atributes

--- app_port/admin.py
+++ app_port/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Channel, Project, Tag
+from .models import Channel, Project, Tag, Profile
 
 # Register your models here.
-admin.site.register([Channel, Project, Tag])
+admin.site.register([Channel, Project, Tag, Profile])
--- app_port/views.py
+++ app_port/views.py
@@ -14,19 +14,39 @@ def edit(request):
     user = request.user
 
     if request.method == 'POST':
+        if "bio_saved" in request.POST:
+            user.first_name = request.POST["first_name"]
+            user.last_name = request.POST["last_name"]
+            user.profile.bio = request.POST["bio"]
+            user.save()
+
         if "projects_saved" in request.POST:
             print(request.POST)
+            projectID = request.POST["projectID"]
+            project = Project.objects.get(id=projectID)
+            project.name = request.POST["project_name"]
+            project.description = request.POST["project_description"]
+            project.url = request.POST["project_url"]
+            project.save()
+
         if "project_new" in request.POST:
             Project.objects.create(name="New Project Title", description="Type your description here.", userID=user,
                                    url = "www.your-url-goes-here.com")
+
         if "project_delete" in request.POST:
             projectID = request.POST["projectID"]
             Project.objects.filter(id=projectID).delete()
 
         if "channels_saved" in request.POST:
-            print(request.POST)
+            channelID = request.POST["channelID"]
+            channel = Channel.objects.get(id=channelID)
+            channel.name = request.POST["channel_name"]
+            channel.url = request.POST["channel_url"]
+            channel.save()
+
         if "channel_new" in request.POST:
             Channel.objects.create(name="Channel name", url="www.your-url-goes-here.com", userID=user)
+
         if "channel_delete" in request.POST:
             channelID = request.POST["channelID"]
             Channel.objects.filter(id=channelID).delete()
