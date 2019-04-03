baffdebb0efcb6c99d50616ada58db996c94b776
Mingyyy <j.yanming@gmail.com>
2019-04-03 17:22:51 +0800

    Add field ready(Boolean, default at False) in Profile models.py

--- app_port/models.py
+++ app_port/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     currentlocation = models.CharField(max_length=50, blank=True)
+    ready=models.BooleanField(default=False)
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
