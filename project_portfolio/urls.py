53c1773588d2e04b5cf6bd557d1474fe89a46ee1
Mingyyy <j.yanming@gmail.com>
2019-04-08 13:25:51 +0800

    Change the name of the project to project_portfolio

--- project_portfolio/urls.py
+++ project_portfolio/urls.py
@@ -1,4 +1,4 @@
-"""project_port URL Configuration
+"""project_portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
